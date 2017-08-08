b257b175486595cfee981784af19df77f1491195
Webb Peng <webb.png@gmail.com>
2017-08-09 01:27:06 +0800

    auto launch chrome by default

--- src/index.js
+++ src/index.js
@@ -2,39 +2,90 @@
 import yargs from 'yargs';
 import { Chromeless } from 'chromeless';
 import termImg from 'term-img';
-import logUpdate from 'log-update';
+import { h, render, Component } from 'ink';
+import { launch as chromeLaunch } from 'chrome-launcher';
 
-const { argv } = yargs.demand(1, 'Please specify URL');
-const url = argv._[0];
+const { argv } = yargs
+	.demand(1, 'Please specify URL')
+	.options({
+		width: {
+			desc: 'width',
+			alias: 'w',
+			type: 'number',
+			default: 320,
+		},
+		height: {
+			desc: 'height',
+			alias: 'h',
+			type: 'number',
+			default: 480,
+		},
+	})
+;
+
+const {
+	_: [url],
+	width = 320,
+	height = 480,
+} = argv;
 
 async function run() {
 
-	const chromeless = new Chromeless({
-		launchChrome: false,
+	await chromeLaunch({
+		chromeFlags: [
+			'--disable-gpu',
+			'--headless',
+		],
+		port: 9222,
 	});
 
-	chromeless
-		.setViewport({ width: 320, height: 240, scale: 1 })
-		.goto(url)
-		// .goto('http://www.html5videoplayer.net/videos/madagascar3.mp4')
-	;
+	const chromeless = new Chromeless({
+		launchChrome: false,
+		viewport: { width, height },
+	});
 
-	const liveDisplay = async () => {
-		try {
-			const screenshotFile = await chromeless.screenshot();
-			logUpdate(termImg.string(screenshotFile));
-		}
-		catch (err) {}
-
-		setTimeout(liveDisplay, 10);
-	};
-
-	liveDisplay();
+	chromeless.goto(url);
 
 	const stop = async () => {
 		try { await chromeless.end(); }
 		catch (err) {}
 	};
+
+
+	class App extends Component {
+		state = {
+			data: '',
+		};
+
+		async componentDidMount() {
+
+			const loop = async () => {
+				try {
+					const screenshotFile = await chromeless.screenshot();
+					this.setState({
+						data: termImg.string(screenshotFile, {
+							width: `${width}px`,
+							height: `${height}px`,
+							preserveAspectRatio: false,
+						}),
+					});
+				}
+				catch (err) {}
+
+				setTimeout(loop, 1000 / 60);
+			};
+			loop();
+		}
+
+		render() {
+			const { data } = this.state;
+			return (
+				<div>{data}</div>
+			);
+		}
+	}
+
+	render(<App />);
 
 	process.on('SIGINT', stop);
 	process.on('SIGTERM', stop);
